74e4bb322f7cbdf625d62350bf0ce65e4224b1a9
WalleyemanZ <jacobzea96@gmail.com>
2023-01-28 15:57:15 -0700

    working though local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,27 +7,27 @@ var targetDivs = $('.time-block')
 console.log(targetDivs);
 
 
+// var hour09 = $('#hour-09').attr('id');
+// console.log(localStorage.getItem(hour09))
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  
+}
+
+
 var displayTime = dayjs().format('dddd MMM D, YYYY');
 $("#currentDay").text(displayTime);
 
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //  
   
   function setColors() {
-    var variableHour = dayjs().format('H')
-    console.log(typeof variableHour);
-    console.log(typeof $(targetDivs[0]).attr('id').replace('hour-', ''));
+    var variableHour = dayjs().format('H');
     
     for (let i = 0; i < targetDivs.length; i++) {
       var hour = $('#hour-' + i);
